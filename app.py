8242215e18697ad0c28814f2201c1dfb24c09a4b
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-04-22 23:35:17 -0700

    Finished adding index route

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import session
+import genomelink
+from api_constants import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    authorize_url 
+  authorize_url = genomelink.OAuth.authorize_url(client_id=my_client_id, scope=[], callback_url=my_callback_url)
+  attributes = ['eye-color', 'beard-thickness', 'morning-person']
+  reports = []
+  if session.get('oauth_token'):
+    for name in attributes:
+      reports.append(genomelink.Report.fetch(name=name, population='European', token=session['oauth_token']))
+  return render_template('index.html', authorize_url=authorize_url, reports=reports)
