06dd52a82b5a5d2590f4eb3a8293a70d7206a6df
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-04-25 23:50:53 -0700

    Added applicable scopes

--- app.py
+++ app.py
@@ -1,49 +1,62 @@
 from flask import Flask, flash, render_template, request, redirect, session, url_for
 import genomelink
 
-
 def create_app():
-    app = Flask(__name__)
+  app = Flask(__name__)
+  interested_scopes = [
+    'report:myocardial-infarction',
+    'report:prostate-cancer',
+    'report:pancreatic-cancer',
+    'report:liver-cancer',
+    'report:breast-cancer',
+    'report:colorectal-cancer',
+    'report:vitamin-b12',
+    'report:vitamin-a',
+    'report:milk-allergy',
+    'report:egg-allergy',
+    'report:alcohol-drinking-behavior'
+  ]
 
-    @app.route('/')
-    def index():
-        authorize_url = genomelink.OAuth.authorize_url(scope=['report:eye-color report:beard-thickness report:morning-person report:alcohol-drinking-behavior report:milk-allergy'])
+  @app.route('/')
+  def index():
+    authorize_url = genomelink.OAuth.authorize_url(scope=interested_scopes)
 
-        # Fetching a protected resource using an OAuth2 token if exists.
-        reports = []
-        if session.get('oauth_token'):
-            for name in ['eye-color', 'beard-thickness', 'morning-person', 'alcohol-drinking-behavior', 'milk-allergy']:
-                reports.append(genomelink.Report.fetch(name=name, population='european', token=session['oauth_token']))
+    # Fetching a protected resource using an OAuth2 token if exists.
+    reports = []
+    attributes = [report.split(':')[-1] for report in interested_scopes]
+    if session.get('oauth_token'):
+        for name in attributes:
+            reports.append(genomelink.Report.fetch(name=name, population='european', token=session['oauth_token']))
 
-        return render_template('index.html', authorize_url=authorize_url, reports=reports)
+    return render_template('index.html', authorize_url=authorize_url, reports=reports)
 
-    @app.route('/callback')
-    def callback():
-        # The user has been redirected back from the provider to your registered
-        # callback URL. With this redirection comes an authorization code included
-        # in the request URL. We will use that to obtain an access token.
-        try:
-            token = genomelink.OAuth.token(request_url=request.url)
-        except genomelink.errors.GenomeLinkError as e:
-            flash('Authorization failed.')
-            if os.environ.get('DEBUG') == '1':
-                flash('[DEBUG] ({}) {}'.format(e.error, e.description))
-            return redirect(url_for('index'))
-
-        # At this point you can fetch protected resources but lets save
-        # the token and show how this is done from a persisted token in index page.
-        session['oauth_token'] = token
+  @app.route('/callback')
+  def callback():
+    # The user has been redirected back from the provider to your registered
+    # callback URL. With this redirection comes an authorization code included
+    # in the request URL. We will use that to obtain an access token.
+    try:
+        token = genomelink.OAuth.token(request_url=request.url)
+    except genomelink.errors.GenomeLinkError as e:
+        flash('Authorization failed.')
+        if os.environ.get('DEBUG') == '1':
+            flash('[DEBUG] ({}) {}'.format(e.error, e.description))
         return redirect(url_for('index'))
 
-    return app
+    # At this point you can fetch protected resources but lets save
+    # the token and show how this is done from a persisted token in index page.
+    session['oauth_token'] = token
+    return redirect(url_for('index'))
+
+  return app
 
 if __name__ == '__main__':
-    # This allows us to use a plain HTTP callback.
-    import os
-    os.environ['DEBUG'] = '1'
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  # This allows us to use a plain HTTP callback.
+  import os
+  os.environ['DEBUG'] = '1'
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-    # Run local server on port 5000.
-    app = create_app()
-    app.secret_key = os.urandom(24)
-    app.run(debug=True)
+  # Run local server on port 5000.
+  app = create_app()
+  app.secret_key = os.urandom(24)
+  app.run(debug=True)
