684583240161fdf558505dc26d44b396739a0e3e
Arjun Bharadwaj <abharadwaj@ucdavis.edu>
2018-04-25 23:47:19 -0700

    Added additional scopes

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, flash, render_template, request, redirect, session, url_for
-from api_constants import *
 import genomelink
-import os
+
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/')
     def index():
-        authorize_url = genomelink.OAuth.authorize_url(client_id=my_client_id, scope=['report:'], callback_url=my_callback_url)
+        authorize_url = genomelink.OAuth.authorize_url(scope=['report:eye-color report:beard-thickness report:morning-person report:alcohol-drinking-behavior report:milk-allergy'])
 
         # Fetching a protected resource using an OAuth2 token if exists.
         reports = []
         if session.get('oauth_token'):
-            for name in ['eye-color', 'beard-thickness', 'morning-person']:
+            for name in ['eye-color', 'beard-thickness', 'morning-person', 'alcohol-drinking-behavior', 'milk-allergy']:
                 reports.append(genomelink.Report.fetch(name=name, population='european', token=session['oauth_token']))
 
         return render_template('index.html', authorize_url=authorize_url, reports=reports)
@@ -24,7 +23,7 @@ def create_app():
         # callback URL. With this redirection comes an authorization code included
         # in the request URL. We will use that to obtain an access token.
         try:
-            token = genomelink.OAuth.token(client_id=my_client_id, client_secret=my_client_secret, callback_url=my_callback_url, request_url=request.url)
+            token = genomelink.OAuth.token(request_url=request.url)
         except genomelink.errors.GenomeLinkError as e:
             flash('Authorization failed.')
             if os.environ.get('DEBUG') == '1':
@@ -40,10 +39,11 @@ def create_app():
 
 if __name__ == '__main__':
     # This allows us to use a plain HTTP callback.
+    import os
     os.environ['DEBUG'] = '1'
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
     # Run local server on port 5000.
     app = create_app()
-    #app.secret_key = os.urandom(24)
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
